43b41601847d5bf54b63aafe593ee13345e473bf
santoshjammi <santoshjammi@gmail.com>
2021-12-07 12:42:54 +0530

    Update installDotNet.ps1

--- installDotNet.ps1
+++ installDotNet.ps1
@@ -1,4 +1,4 @@
-Param([String]$temp)
+$temp=$env:TEMP
 powershell.exe Invoke-WebRequest -OutFile $temp\ndp48-web.exe -Uri http://download.visualstudio.microsoft.com/download/pr/7afca223-55d2-470a-8edc-6a1739ae3252/c9b8749dd99fc0d4453b2a3e4c37ba16/ndp48-web.exe
 
 $args=('/passive', '/norestart')
